2fbc5d7bf0117c3251ac8d73505479ede1a2bfa7
xaythideth <sourivongx@gmail.com>
2024-07-09 22:36:46 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Maincomponent from './component/Maincomponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Maincomponent/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ component/basic.js
@@ -0,0 +1,11 @@
+function Maincomponent() {
+    return (
+      <View>
+  
+              <Text>xaythideth sourivong</Text>
+              <Image source={{
+                  uri:'https://i.pinimg.com/564x/97/c6/50/97c650c6de8174301f5c5eea3da35a8a.jpg'
+              }}/>
+      </View>
+    )
+  }
